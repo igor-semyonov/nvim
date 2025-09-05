7d3f00540ec79d65270e44af26e05addf47186e7
Igor Semyonov <igor@semyonov.xyz>
2025-09-05 14:05:42 -0400

    changed indent char

--- plugin/options.lua
+++ plugin/options.lua
@@ -17,7 +17,7 @@ local options = {
 	mouse = "", -- allow the mouse to be used in neovim
 	pumheight = 6, -- pop up menu height
 	showmode = false, -- we don't need to see things like -- INSERT -- anymore
-    showtabline = 2,                         -- always show tabs
+	showtabline = 2, -- always show tabs
 	autoindent = true,
 	smartindent = true, -- make indenting smarter again
 	splitbelow = true, -- force all horizontal splits to go below current window
@@ -32,12 +32,12 @@ local options = {
 	shiftwidth = 4, -- the number of spaces inserted for each indentation
 	tabstop = 4, -- insert 2 spaces for a tab
 	-- softtabstop = 4,
-    cursorline = true, -- highlight the current line
-    cursorcolumn = true, -- highlight the current line
+	cursorline = true, -- highlight the current line
+	cursorcolumn = true, -- highlight the current line
 	number = true, -- set numbered lines
 	relativenumber = true, -- set relative numbered lines
 	numberwidth = 2, -- set number column width to 2 {default 4}
-    inccommand = "split",
+	inccommand = "split",
 
 	signcolumn = "yes", -- always show the sign column, otherwise it would shift the text each time
 	wrap = true, -- display lines as one long line
@@ -91,10 +91,11 @@ nvim_create_autocmd({ "BufReadPost" }, {
 
 opt.list = true
 local space = "☠"
+
 opt.listchars:append({
 	eol = "↲",
 	tab = "├─",
-	leadmultispace = "├───",
+	leadmultispace = "│   ",
 	-- multispace = space,
 	trail = space,
 	-- nbsp = space
@@ -117,3 +118,71 @@ nvim_create_autocmd("InsertLeave", {
 		nvim_set_hl(0, "TrailingWhitespace", { link = "Error" })
 	end,
 })
+
+-- Set the char for the indent line
+-- vim.g.indentline_char = "|"
+--     -- By default I am using space style indentation and 2 spaces for it
+--     -- vim.o.listchars = "trail:•,extends:#,nbsp:.,precedes:❮,extends:❯,tab:› ,leadmultispace:"
+--     .. vim.g.indentline_char
+--     .. "  "
+
+-- -- I am using tab and leadmultispace in listchars to display the indent line. The chars for tab and
+-- -- leadmultispace should be updated based on whether the indentation has been changed.
+-- -- * If using space as indentation: set tab to a special character for denotation and leadmultispace
+-- -- to the indent line character followed by multiple spaces whose amounts depends on the number of
+-- -- spaces to use in each step of indent.
+-- -- * If using tab as indentation: set leadmultispace to a special character for denotation and tab
+-- -- to the indent line character.
+-- local function update_indent_char(is_local)
+--     local listchars_update = function(items)
+--         local listchars = vim.api.nvim_get_option_value("listchars", {})
+--         for item, val in pairs(items) do
+--             if listchars:match(item) then
+--                 listchars = listchars:gsub("(" .. item .. ":)[^,]*", "%1" .. val)
+--             else
+--                 listchars = listchars .. "," .. item .. ":" .. val
+--             end
+--         end
+--         return listchars
+--     end
+--     local new_listchars = ""
+--     if vim.api.nvim_get_option_value("expandtab", {}) then
+--         local spaces = vim.api.nvim_get_option_value("shiftwidth", {})
+--         -- When shiftwidth is 0, vim will use tabstop value
+--         if spaces == 0 then
+--             spaces = vim.api.nvim_get_option_value("tabstop", {})
+--         end
+--         new_listchars = listchars_update({
+--             tab = "› ",
+--             leadmultispace = vim.g.indentline_char .. string.rep(" ", spaces - 1),
+--         })
+--     else
+--         new_listchars = listchars_update({
+--             tab = vim.g.indentline_char .. " ",
+--             leadmultispace = "␣",
+--         })
+--     end
+--     local opts = {}
+--     if is_local then
+--         opts.scope = "local"
+--     end
+--     vim.api.nvim_set_option_value("listchars", new_listchars, opts)
+-- end
+
+-- vim.api.nvim_create_augroup("indent_line", { clear = true })
+-- vim.api.nvim_create_autocmd({ "OptionSet" }, {
+--     group = "indent_line",
+--     pattern = { "shiftwidth", "expandtab", "tabstop" },
+--     callback = function()
+--         update_indent_char(vim.v.option_type == "local")
+--     end,
+-- })
+-- -- OptionSet is not triggered on startup
+-- -- This may be not needed. The listchars has been set properly in options.vim and it will be sourced
+-- -- on startup.
+-- vim.api.nvim_create_autocmd({ "VimEnter" }, {
+--     group = "indent_line",
+--     callback = function()
+--         update_indent_char(false)
+--     end,
+-- })
