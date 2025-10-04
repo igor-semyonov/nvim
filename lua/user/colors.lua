1d04ad077001d138e75fdd9cc5f79fd33a395bea
Igor Semyonov <igor@semyonov.xyz>
2025-10-03 22:59:02 -0400

    updated colors and illuminate

--- lua/user/colors.lua
+++ lua/user/colors.lua
@@ -5,57 +5,115 @@ local monk = "#80FF80"
 local monk_inverse = "#800080"
 
 require("nightfox").setup({
-    options = {
-        transparent = true,
-        styles = {             -- Style to be applied to different syntax groups
-            comments = "NONE", -- Value is any valid attr-list value `:help attr-list`
-            conditionals = "NONE",
-            constants = "NONE",
-            functions = "NONE",
-            keywords = "bold",
-            numbers = "bold",
-            operators = "NONE",
-            strings = "NONE",
-            types = "NONE",
-            variables = "NONE",
-        },
-        inverse = {
-            match_paren = true,
-            search = false,
-            visual = true,
-        },
-    },
-    palettes = {
-        all = {},
-    },
-    groups = {
-        all = {
-            LineNr = { fg = bright_fg, bg = "black", },
-            SignColumn = { fg = bright_fg, bg = "black", },
-            -- LineNr = { fg = bright_fg, bg = "#000000", style = "bold" },
-            -- LineNrAbove = { fg = bright_fg, bg = dark_grey },
-            -- LineNrBelow = { fg = bright_fg, bg = dark_grey },
-            -- FoldColumn = { fg = bright_fg, bg = "#8000FF" },
-            Cursor = { bg = "#FF30FF" },
-            CursorLine = { bg = crosshair },
-            CursorColumn = { bg = crosshair },
-            CursorLineNr = { bg = monk, fg = monk_inverse },
-            -- Folded = { bg = "orange" },
-            Folded = { bg = "#FF4040" },
-            NormalFloat = { bg = "black" },
-            FloatBorder = { bg = "black", fg = monk },
-            illuminatedWordText = { fg = "#80FF80" , bg="#E06040"},
+	options = {
+		transparent = true,
+		styles = { -- Style to be applied to different syntax groups
+			comments = "NONE", -- Value is any valid attr-list value `:help attr-list`
+			conditionals = "NONE",
+			constants = "NONE",
+			functions = "NONE",
+			keywords = "bold",
+			numbers = "bold",
+			operators = "NONE",
+			strings = "NONE",
+			types = "NONE",
+			variables = "NONE",
+		},
+		inverse = {
+			match_paren = true,
+			search = false,
+			visual = true,
+		},
+	},
+	palettes = {
+		all = {},
+	},
+	groups = {
+		all = {
+			LineNr = { fg = bright_fg, bg = "black" },
+			SignColumn = { fg = bright_fg, bg = "black" },
+			-- LineNr = { fg = bright_fg, bg = "#000000", style = "bold" },
+			-- LineNrAbove = { fg = bright_fg, bg = dark_grey },
+			-- LineNrBelow = { fg = bright_fg, bg = dark_grey },
+			-- FoldColumn = { fg = bright_fg, bg = "#8000FF" },
+			Cursor = { bg = monk_inverse },
+			CursorLine = { bg = crosshair },
+			CursorColumn = { bg = crosshair },
+			CursorLineNr = { bg = monk, fg = monk_inverse },
+			-- Folded = { bg = "orange" },
+			Folded = { bg = "#FF4040" },
+			NormalFloat = { bg = "black" },
+			FloatBorder = { bg = "black", fg = monk },
+			illuminatedWordText = {fg="#202020", bg = "#2020FF" },
 
-            -- diagnostics
-            NumError = {bg="#FF80FF"},
-            LineError = {bg="#804080"},
-            NumWarn = {bg="#FFA500"},
-            NumInfo = {bg="#FFEA00"},
-            NumHint = {bg="#0020FF"},
-        },
-    },
+			-- diagnostics
+			NumError = { bg = "#FF3030" },
+			LineError = { bg = "#FF0000" },
+			NumWarn = { bg = "#FFA500" },
+			NumInfo = { bg = "#FFEA00" },
+			NumHint = { bg = "#0020FF" },
+		},
+	},
 })
 
 vim.cmd("colorscheme terafox")
 
 require("colorizer").setup()
+require("illuminate").configure({
+	-- providers: provider used to get references in the buffer, ordered by priority
+	providers = {
+		"lsp",
+		"treesitter",
+		"regex",
+	},
+	-- delay: delay in milliseconds
+	delay = 50,
+	-- filetype_overrides: filetype specific overrides.
+	-- The keys are strings to represent the filetype while the values are tables that
+	-- supports the same keys passed to .configure except for filetypes_denylist and filetypes_allowlist
+	filetype_overrides = {},
+	-- filetypes_denylist: filetypes to not illuminate, this overrides filetypes_allowlist
+	filetypes_denylist = {
+		"dirbuf",
+		"dirvish",
+		"fugitive",
+	},
+	-- filetypes_allowlist: filetypes to illuminate, this is overridden by filetypes_denylist
+	-- You must set filetypes_denylist = {} to override the defaults to allow filetypes_allowlist to take effect
+	filetypes_allowlist = {},
+	-- modes_denylist: modes to not illuminate, this overrides modes_allowlist
+	-- See `:help mode()` for possible values
+	modes_denylist = {},
+	-- modes_allowlist: modes to illuminate, this is overridden by modes_denylist
+	-- See `:help mode()` for possible values
+	modes_allowlist = {},
+	-- providers_regex_syntax_denylist: syntax to not illuminate, this overrides providers_regex_syntax_allowlist
+	-- Only applies to the 'regex' provider
+	-- Use :echom synIDattr(synIDtrans(synID(line('.'), col('.'), 1)), 'name')
+	providers_regex_syntax_denylist = {},
+	-- providers_regex_syntax_allowlist: syntax to illuminate, this is overridden by providers_regex_syntax_denylist
+	-- Only applies to the 'regex' provider
+	-- Use :echom synIDattr(synIDtrans(synID(line('.'), col('.'), 1)), 'name')
+	providers_regex_syntax_allowlist = {},
+	-- under_cursor: whether or not to illuminate under the cursor
+	under_cursor = false,
+	-- large_file_cutoff: number of lines at which to use large_file_config
+	-- The `under_cursor` option is disabled when this cutoff is hit
+	large_file_cutoff = 10000,
+	-- large_file_config: config to use for large files (based on large_file_cutoff).
+	-- Supports the same keys passed to .configure
+	-- If nil, vim-illuminate will be disabled for large files.
+	large_file_overrides = nil,
+	-- min_count_to_highlight: minimum number of matches required to perform highlighting
+	min_count_to_highlight = 1,
+	-- should_enable: a callback that overrides all other settings to
+	-- enable/disable illumination. This will be called a lot so don't do
+	-- anything expensive in it.
+	should_enable = function(bufnr)
+		return true
+	end,
+	-- case_insensitive_regex: sets regex case sensitivity
+	case_insensitive_regex = false,
+	-- disable_keymaps: disable default keymaps
+	disable_keymaps = false,
+})
