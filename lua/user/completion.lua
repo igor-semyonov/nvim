2b05e39e136bb5fc6a5b254acf241817d1530a5f
Igor Semyonov <igor@semyonov.xyz>
2026-02-24 14:58:04 -0500

    fixed lspkind

--- lua/user/completion.lua
+++ lua/user/completion.lua
@@ -9,7 +9,51 @@ if not snip_status_ok then
 end
 
 local lspkind = require("lspkind")
-lspkind.init({})
+lspkind.init({
+	-- defines how annotations are shown
+	-- default: symbol
+	-- options: 'text', 'text_symbol', 'symbol_text', 'symbol'
+	mode = "symbol_text",
+
+	-- default symbol map
+	-- can be either 'default' (requires nerd-fonts font) or
+	-- 'codicons' for codicon preset (requires vscode-codicons font)
+	--
+	-- default: 'default'
+	-- preset = "codicons",
+	preset = "default",
+
+	-- override preset symbols
+	--
+	-- default: {}
+	symbol_map = {
+		Text = "󰉿",
+		Method = "󰆧",
+		Function = "󰊕",
+		Constructor = "",
+		Field = "󰜢",
+		Variable = "󰀫",
+		Class = "󰠱",
+		Interface = "",
+		Module = "",
+		Property = "󰜢",
+		Unit = "󰑭",
+		Value = "󰎠",
+		Enum = "",
+		Keyword = "󰌋",
+		Snippet = "",
+		Color = "󰏘",
+		File = "󰈙",
+		Reference = "󰈇",
+		Folder = "󰉋",
+		EnumMember = "",
+		Constant = "󰏿",
+		Struct = "󰙅",
+		Event = "",
+		Operator = "󰆕",
+		TypeParameter = "",
+	},
+})
 
 require("luasnip/loaders/from_vscode").lazy_load()
 
@@ -136,28 +180,42 @@ cmp.setup({
 	-- },
 	formatting = {
 		expandable_indicator = true,
-		fields = { "kind", "abbr", "menu" },
-		format = function(entry, vim_item)
-			vim_item = lspkind.cmp_format({
-				mode = "symbol",
-				menu = {
-					buffer = "[Buffer]",
-					nvim_lsp = "[LSP]",
-					luasnip = "[LuaSnip]",
-					nvim_lua = "[Lua]",
-					latex_symbols = "[Latex]",
-				},
-			})(entry, vim_item)
-			vim_item.abbr = string.sub(vim_item.abbr, 1, 16)
-			return vim_item
-		end,
+		-- fields = { "kind", "abbr", "menu" },
+		-- fields = { "abbr", "icon", "kind", "menu" },
+		fields = { "icon", "abbr", "menu" },
+		format = lspkind.cmp_format({
+			mode = "symbol",
+			menu = {
+				buffer = "[Buf]",
+				nvim_lsp = "[LSP]",
+				luasnip = "[Snip]",
+				nvim_lua = "[Lua]",
+				latex_symbols = "[LTX]",
+			},
+			maxwidth = {
+				-- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
+				-- can also be a function to dynamically calculate max width such as
+				-- menu = function() return math.floor(0.45 * vim.o.columns) end,
+				menu = 8, -- leading text (labelDetails)
+				abbr = 20, -- actual suggestion item
+			},
+			ellipsis_char = "…", -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
+			-- show_labelDetails = true, -- show labelDetails in menu. Disabled by default
+
+			-- The function below will be called before any actual modifications from lspkind
+			-- so that you can provide more controls on popup customization. (See [#30](https://github.com/onsails/lspkind-nvim/pull/30))
+			before = function(entry, vim_item)
+				-- ...
+				return vim_item
+			end,
+		}),
 	},
 	sources = {
 		{ name = "nvim_lsp" },
 		{ name = "nvim_lua" },
+        { name = "path" },
 		{ name = "luasnip" },
 		{ name = "buffer" },
-		{ name = "path" },
 		{ name = "nvim_lsp_signature_help" }, -- display function signatures with current parameter emphasized
 		{ name = "calc" },
 	},
